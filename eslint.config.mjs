152fbf21230802e42d5e5d1e5a1c955e446b7896
Karem Shabana <km425360@gmail.com>
2024-12-21 15:31:55 +0200

    chore: add Prettier configuration and ignore files for formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
@@ -7,7 +7,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    },
   }),
 ];
 
