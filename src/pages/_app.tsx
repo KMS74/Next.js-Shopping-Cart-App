5380b53936fbb3de2accce7ee66cd99375cd4cc9
Karem Shabana <km425360@gmail.com>
2024-12-21 20:28:06 +0200

    refactor: streamline _app.tsx by simplifying Container usage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from 'next/app';
-import Container from '@mui/material/Container';
 import { CartProvider } from '@/CartContext';
 import ResponsiveAppBar from '@/components/Navbar';
+import { Container } from '@mui/material';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
       <ResponsiveAppBar />
-      <Container
-        style={{
-          marginTop: '100px',
-        }}
-      >
+      <Container>
         <Component {...pageProps} />
       </Container>
     </CartProvider>
